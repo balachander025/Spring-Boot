6eb900c86cf54ad82e82611b2f14d8007a3d7452
Balachander <166808771+balachander025@users.noreply.github.com>
2025-07-13 12:29:14 +0530

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package com.bala.two;
+
+
+public class Animal {
+    protected String name;
+
+    public Animal(String name){
+        this.name=name;
+    }
+    public String makesound(){
+        return "Animal makes a sound";
+    }
+
+
+}
